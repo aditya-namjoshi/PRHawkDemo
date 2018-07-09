e292ab96987938896230108d085a0cf373d518ee
aditya-namjoshi <41021912+aditya-namjoshi@users.noreply.github.com>
2018-07-09 14:20:34 -0400

    Add files via upload

---
+++ PRHawkRestService/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PRHawkRestService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Landing page controller
+        // The view calls the Username Web API controller on button click
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PRHawkRestService/Controllers/UsernameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PRHawkRestService.Models;
+using PRHawkRestService.Services;
+using Newtonsoft.Json;
+using PRHawkRestService.WebSettings;
+
+namespace PRHawkRestService.Controllers
+{
+    public class UsernameController : ApiController
+    {
+        private IRepositoryService _repositoryService;
+
+        // Default constructor
+        public UsernameController()
+        {
+            var jsonSettings = new JsonSerializerSettings();
+            jsonSettings.ContractResolver = new RepositoryContractResolver();
+            var webSettings = new WebConfigSettings();
+            _repositoryService = new RepositoryService(jsonSettings, webSettings);
+        }
+
+        // Constructor for unit testing and for dependency injection
+        public UsernameController(IRepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        // Return list of repositories for a user sorted on open pull requests
+        // /api/username/{id}
+        public IEnumerable<Repository> Get(string id)
+        {
+            var repositories = _repositoryService.GetAllRepositoriesForUser(id);
+            _repositoryService.GetOpenPullRequestsForRepositories(repositories, id);
+            return repositories;
+        }
+    }
+}
